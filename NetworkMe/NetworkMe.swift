9c6c80f9a59b8d715126b4fb823dd1c3f98b380e
Ilias Pavidakis <ipavlidakis@gmail.com>
2019-06-02 16:05:49 +0100

    Initial Commit

---
+++ NetworkMe/NetworkMe.swift
@@ -0,0 +1,11 @@
+//
+//  NetworkMe.swift
+//  NetworkMe
+//
+//  Created by Ilias Pavlidakis on 02/06/2019.
+//  Copyright © 2019 Ilias Pavlidakis. All rights reserved.
+//
+
+class NetworkMe {
+
+}
