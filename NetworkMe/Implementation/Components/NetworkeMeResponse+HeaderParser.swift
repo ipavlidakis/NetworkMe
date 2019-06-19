98ef6bb47eb2d6ed58fd117da25c4eb01707b9f8
Ilias Pavidakis <ipavlidakis@gmail.com>
2019-06-19 21:05:38 +0100

    index on develop: 46861f1 Merge branch 'develop' of https://github.com/ipavlidakis/NetworkMe into develop

---
+++ NetworkMe/Implementation/Components/NetworkeMeResponse+HeaderParser.swift
@@ -0,0 +1,34 @@
+//
+//  NetworkeMeResponse+HeaderParser.swift
+//  NetworkMe
+//
+//  Created by Ilias Pavlidakis on 08/06/2019.
+//  Copyright © 2019 Ilias Pavlidakis. All rights reserved.
+//
+
+import Foundation
+
+extension NetworkMe.Response {
+
+    struct HeaderParser: NetworkMeHeaderParserProtocol {
+
+        func parseHeaders(from response: URLResponse?) -> [NetworkMe.Header.Response] {
+
+            guard
+                let httpResponse = response as? HTTPURLResponse,
+                !httpResponse.allHeaderFields.isEmpty
+            else {
+                return []
+            }
+
+            return httpResponse.allHeaderFields
+                .compactMap {
+                    guard
+                        let stringKey = $0.key as? String,
+                        let stringValue = $0.value as? String
+                    else { return nil }
+                    return NetworkMe.TransformableKeyValuePair(stringKey, stringValue) }
+                .compactMap { NetworkMe.Header.Response.init(rawValue: $0) }
+        }
+    }
+}
